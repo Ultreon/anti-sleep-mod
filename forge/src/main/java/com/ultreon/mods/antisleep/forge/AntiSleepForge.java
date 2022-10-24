62862c5cca604d9d909838a6ca758c2f6332d3d5
Qboi123 <qboiwastaken@gmail.com>
2022-10-24 22:47:25 +0200

    Left over Arch API code that's unused.

--- forge/src/main/java/com/ultreon/mods/antisleep/forge/AntiSleepForge.java
+++ forge/src/main/java/com/ultreon/mods/antisleep/forge/AntiSleepForge.java
@@ -1,15 +1,11 @@
 package com.ultreon.mods.antisleep.forge;
 
-import dev.architectury.platform.forge.EventBuses;
 import com.ultreon.mods.antisleep.AntiSleep;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(AntiSleep.MOD_ID)
 public class AntiSleepForge {
     public AntiSleepForge() {
-        // Submit our event bus to let architectury register our content on the right time
-        EventBuses.registerModEventBus(AntiSleep.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         AntiSleep.init();
     }
 }
