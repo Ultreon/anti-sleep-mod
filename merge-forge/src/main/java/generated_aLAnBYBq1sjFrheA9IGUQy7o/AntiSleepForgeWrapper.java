3f4794a7f8f91f5205584508c66cf4d5435c10a3
Qboi123 <qboiwastaken@gmail.com>
2022-10-25 15:32:26 +0200

    Fix forge wrapper classname, and add a build run config.

---
+++ merge-forge/src/main/java/generated_aLAnBYBq1sjFrheA9IGUQy7o/AntiSleepForgeWrapper.java
@@ -0,0 +1,10 @@
+package generated_aLAnBYBq1sjFrheA9IGUQy7o;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("antisleep_forge")
+public class AntiSleepForgeWrapper {
+    public AntiSleepForgeWrapper() {
+
+    }
+}
